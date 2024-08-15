01936719bc2a8c89ae72cf3783f5ea4445525529
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-08-15 04:28:07 +0200

    chore: Add Service page with navbar, footer, and about us section

---
+++ src/app/services/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import Image from 'next/image'
+import AboutUsSection from '../components/AboutUsSection'
+
+export default function Service() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <main className="flex-grow">
+        <section className="relative h-screen">
+          <Image
+            src="/assets/home.jpg"
+            alt="Background"
+            layout="fill"
+            objectFit="cover"
+            className="brightness-50"
+          />
+          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+          <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+            <h1 className="text-6xl font-bold mb-4">Services</h1>
+          </div>
+        </section>
+        <AboutUsSection/>
+      </main>
+      <Footer/>
+      </div>
+  )
+}
